fc6afe7fa921db3ae6aed999e07b305b6c52090b
fr4nlearning <francis.informatica@gmail.com>
2021-12-30 13:30:13 +0100

    First commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Clase principal
+ * @author caspol
+ *
+ */
+public class Main {
+
+	/**
+	 * Main methods
+	 * @param args arguments of app
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
